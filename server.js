307b5dd08ac551f676f9118bcfb1cfb1e839fa3c
JalalHammash <jalalhammash200@gmail.com>
2024-08-20 10:18:11 +0300

    changing database hosting to railway

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: `postgresql://jalal_postgresql_user:Xofey8mBYChofiKRwB51U73FwpnIKnxZ@dpg-cr0r333tq21c73ckb580-a.oregon-postgres.render.com/jalal_postgresql`,
+    connectionString: `postgresql://postgres:gESsJgzjeQSUfwVjAFMjaUsfbQjVLshN@junction.proxy.rlwy.net:14213/railway`,
     ssl: true 
   },
 });
